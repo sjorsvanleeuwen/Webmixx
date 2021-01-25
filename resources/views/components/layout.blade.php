cdaa29b4df6046407ed1e5f4b5dcb36853f20fa8
Sjors van Leeuwen <sjors@sjorsvanleeuwen.com>
2021-01-25 19:41:20 +0100

    Menu fix

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,12 +9,12 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link rel="stylesheet" href="/vendor/webmixx/css/webmixx.css">
+    <link rel="shortcut icon" href="data:image/x-icon;," type="image/x-icon">
     <title>Webmixx</title>
 </head>
 <body>
+    <x-webmixx::menu />
     <div class="container @if(isset($withoutVue) && $withoutVue === true) no-vue @endif" id="app">
-        <x-webmixx::menu />
-
         {!! $slot !!}
     </div>
     <script src="/vendor/webmixx/js/manifest.js" defer></script>
--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -1,30 +1,28 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">Webmixx</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav">
+    <a class="navbar-brand" href="#">Webmixx</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="{{ route('webmixx.dashboard') }}">Home</a>
+            </li>
+            @can('viewAny', \SjorsvanLeeuwen\Webmixx\Models\Page::class)
                 <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="{{ route('webmixx.dashboard') }}">Home</a>
+                    <a class="nav-link" aria-current="page" href="{{ route('webmixx.pages.index') }}">Pages</a>
                 </li>
-                @can('viewAny', \SjorsvanLeeuwen\Webmixx\Models\Page::class)
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="{{ route('webmixx.pages.index') }}">Pages</a>
-                    </li>
-                @endcan
-                @can('viewAny', \SjorsvanLeeuwen\Webmixx\Models\PageTemplate::class)
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="{{ route('webmixx.page_templates.index') }}">Page Templates</a>
-                    </li>
-                @endcan
-                @can('viewAny', \SjorsvanLeeuwen\Webmixx\Models\PageAttributeTemplate::class)
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="{{ route('webmixx.page_attribute_templates.index') }}">Page Attribute Templates</a>
-                    </li>
-                @endcan
-            </ul>
-        </div>
+            @endcan
+            @can('viewAny', \SjorsvanLeeuwen\Webmixx\Models\PageTemplate::class)
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="{{ route('webmixx.page_templates.index') }}">Page Templates</a>
+                </li>
+            @endcan
+            @can('viewAny', \SjorsvanLeeuwen\Webmixx\Models\PageAttributeTemplate::class)
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="{{ route('webmixx.page_attribute_templates.index') }}">Page Attribute Templates</a>
+                </li>
+            @endcan
+        </ul>
     </div>
 </nav>
